0e10c230b5fcd797dc02338b18e23bfd71d84dbf
Richard JEON <ztzy1907@gmail.com>
2022-07-16 00:05:03 +0900

    feat: RestController for Event Logging

---
+++ src/main/java/com/rich/jenkins/EventController.java
@@ -0,0 +1,24 @@
+package com.rich.jenkins;
+
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rich-event")
+public class EventController {
+
+    private final Logger logger = LoggerFactory.getLogger(EventController.class);
+
+    @PostMapping
+    public ResponseEntity<Void> response(@RequestBody Map<String, Object> requestBody) {
+        logger.info(String.valueOf(requestBody));
+
+        return ResponseEntity.ok().build();
+    }
+}
